706138e1ab34f452e0bab1198291e05ac71d6220
Rémi Becheras <rbecheras@sirap.fr>
2018-01-30 16:50:20 +0100

    create gulp karma task as a workaround for #13, to run karma start singleRun on any file change

---
+++ scripts/gulp-tasks/karma.js
@@ -0,0 +1,49 @@
+const path = require('path')
+
+const gulp = require('gulp')
+const watch = require('gulp-watch')
+const shell = require('shelljs')
+
+// Change working dir to come back to the project root
+const workingDir = path.resolve(path.join(__dirname, '../../'))
+process.chdir(workingDir)
+
+/**
+ * Watch all js files in src and run `pipeline:tests` on any change
+ * @todo split it to several tasks for each kind of source files (src/lib, src/tests, src/docs, scripts, package.json)
+ */
+
+gulp.task('watch:karma', function (done) {
+  const files = [
+    'package.json',
+    'webpack.config.js',
+    'karma.config.js',
+
+    'src/lib/**/*.js',
+    'src/tests/**/*.js',
+    'src/assets/**/*.*',
+
+    'scripts/**/*.js'
+  ]
+
+  const options = {
+    ignoreInitial: true, // default
+    events: ['add', 'change', 'unlink'], // default
+    // events: ['add', 'change', 'unlink', 'addDir', 'unlinkDir', 'error', 'ready', 'raw'] // all
+    base: workingDir,
+    name: 'watcher-all-src',
+    verbose: true,
+    readDelay: 10, // default
+    read: false
+  }
+
+  const build = cb => shell.exec('yarn karma start', cb)
+
+  const onChange = vinyl => {
+    build()
+  }
+
+  build(() => watch(files, options, onChange))
+})
+
+gulp.task('default', ['watch:karma'])
