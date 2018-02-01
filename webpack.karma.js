48135775b547c37345607453f630f2147051af05
Rémi Becheras <rbecheras@sirap.fr>
2018-02-01 07:07:25 +0100

    refs #11: configure coverage instrumentation registering istanbul-instrumenter as webpack loader in webpack config for karma

--- webpack.karma.js
+++ webpack.karma.js
@@ -1,9 +1,16 @@
+const path = require('path')
 const webpackConfig = require('./webpack.common')
 
-module.exports = Object.assign({
-  module: Object.assign({
-    // Suppress warning from mocha: "Critical dependency: the request of a dependency is an expression"
-    // @see https://webpack.js.org/configuration/module/#module-contexts
-    exprContextCritical: false
-  }, webpackConfig.module)
-}, webpackConfig)
+// Suppress warning from mocha: "Critical dependency: the request of a dependency is an expression"
+// @see https://webpack.js.org/configuration/module/#module-contexts
+webpackConfig.module.exprContextCritical = false
+
+// add loader: istanbul-instrumenter
+webpackConfig.module.rules.push({
+  // instrument only testing sources with Istanbul
+  test: /\.js$/,
+  use: { loader: 'istanbul-instrumenter-loader' },
+  include: path.resolve('src/components/')
+})
+
+module.exports = webpackConfig
